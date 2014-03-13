4fe16d028738aa8a222093740fdeb71d8dfbbb51
cxs <nothi1992@gmail.com>
2014-03-13 16:50:36 +0800

    完成Book类

---
+++ common/model/book.py
@@ -0,0 +1,15 @@
+__author__ = 'nothi'
+class Book(object):
+    attrs = ["isbn", "price", "title","author", "press", "desc", "cover", "link", "platform", "instant_price","time"]
+    def __init__(self):
+        self.isbn = ''
+        self.price = 0.0
+        self.title = ''
+        self.author = ''
+        self.press = ''
+        self.desc = ''
+        self.cover = ''
+        self.link = ''
+        self.platform = ''
+        self.instant_price = ''
+        self.time = 0
