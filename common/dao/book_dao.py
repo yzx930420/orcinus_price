3d6dd14f6d752a3a6f34abc90e8a71398b4bce5c
yzx930420 <yzx930420@gmail.com>
2014-03-13 17:06:32 +0800

    add book_dao.py

---
+++ common/dao/book_dao.py
@@ -0,0 +1,13 @@
+__author__ = 'yzx930420'
+
+from common.model.book import Book
+
+class BookDAO():
+    def insert(self, book):
+        pass
+
+    def query(self, map):
+        pass
+
+    def query(self, map, start_time, end_time):
+        pass
