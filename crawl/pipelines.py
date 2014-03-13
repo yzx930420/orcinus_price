90516ab69fbb1ff7738982f0d6209ca9536954be
ArchMage <lfj200411@163.com>
2014-03-14 03:33:44 +0800

    整合爬虫

---
+++ crawl/pipelines.py
@@ -0,0 +1,23 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+class TestPipeline(object):
+	con = pymongo.Connection("localhost", 27017)
+	db = con.bestbuyer
+	def process_item(self, item, spider):
+		if(len(item['ISBN']) == 0):
+			return item
+		dbdata = {"name":"","price":"","ISBN":"","author":"","press":""}
+		dbdata["name"] = item["name"][0]
+		dbdata["price"] = item["price"][0]
+		dbdata["ISBN"] = item["ISBN"][0]
+		dbdata["author"] = item["author"][0]
+		dbdata["press"] = item["press"][0]
+		try:
+			self.db.bookInfo.insert(dbdata)
+		except:
+			print "====================================wocao"
+
+		return item
---
+++ crawl/settings.py
@@ -0,0 +1,17 @@
+# Scrapy settings for test project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'crawl'
+
+SPIDER_MODULES = ['crawl.spiders']
+NEWSPIDER_MODULE = 'crawl.spiders'
+ITEM_PIPELINES = ['crawl.pipelines.dangdangPipeline']
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36'
+LOG_FILE = 'C:/Users/Administrator/test/obj/Debug/error.txt'
+LOG_ENABLED = False
