cc6ab0e25786144491896c6c448c9beb57d2230e
XianSen <946007236@qq.com>
2014-03-15 22:36:10 +0800

    修改book_dao

--- common/dao/book_dao.py
+++ common/dao/book_dao.py
@@ -11,15 +11,16 @@ from common.model.book_info import BookInfo as  BookPO
 
 class BookDAO():
     def __init__(self):
-        self.conn = MySQLdb.connect(common.dao.settings.MYSQL_URL,
-                                    common.dao.settings.MYSQL_USER,
-                                    common.dao.settings.MYSQL_PASSWORD,
-                                    common.dao.settings.MYSQL_DATABASE)
+        print "ok"
+        self.conn = MySQLdb.connect(host=common.dao.settings.MYSQL_URL,
+                                    user=common.dao.settings.MYSQL_USER,
+                                    passwd=common.dao.settings.MYSQL_PASSWORD,
+                                    db=common.dao.settings.MYSQL_DATABASE)
         self.cursor = self.conn.cursor()
+        print "你妹"
 
-    def __parse_book_to_po(book_1):
+    def __parse_book_to_po(self, book):
         #把Book转化为两个PO实体
-        book = Book()
         #steop1 : book->goods
         goods = GoodsPO()
         goods.isbn = book.isbn
@@ -42,58 +43,76 @@ class BookDAO():
         book = Book()
 
         # bookpo->book
-        book['isbn'] = bookpo['isbn']
-        book['price'] = bookpo['price']
-        book['title'] = bookpo['title']
-        book['author'] = bookpo['author']
-        book['press'] = bookpo['press']
-        book['description'] = bookpo['description']
-        book['cover'] = bookpo['cover']
+        book.isbn = bookpo.isbn
+        book.price = bookpo.price
+        book.title = bookpo.title
+        book.author = bookpo.author
+        book.press = bookpo.press
+        book.description = bookpo.description
+        book.cover = bookpo.cover
 
         #goodspo->book
-        book['link'] = goodspo['link']
-        book['platform'] = goodspo['platform']
-        book['instant_price'] = goodspo['instant_price']
-        book['crawling_time'] = goodspo['crawling_time']
+        book.link = goodspo.link
+        book.platform = goodspo.platform
+        book.instant_price = goodspo.instant_price
+        book.crawling_time = goodspo.crawling_time
 
         return book
 
     def insert(self, book):
-        goods,bookpo = BookDAO.__parse_book_to_po(book)
+        goods,bookpo = self.__parse_book_to_po(book)
         #插入goods(isbn, instant_price, link, platform, crawling_time)
         statement = 'insert into book_goods_info(isbn, instant_price, link, platform,crawling_time) values(%s, %s, %s, %s, %s)'
         self.cursor.execute(statement, [goods.isbn,goods.instant_price, goods.link, goods.platform, goods.crawling_time])
-        self.cursor.commit()
+        statement_book_info = "insert into book_info(isbn, price, title, author, press, description, cover) values(%s,%s,%s,%s,%s,%s,%s)"
+        self.cursor.execute(statement_book_info, [bookpo.isbn,
+                                        bookpo.price,
+                                        bookpo.title,
+                                        bookpo.author,
+                                        bookpo.press,
+                                        bookpo.description,
+                                        bookpo.cover])
+        self.conn.commit()
 
     def query(self, pair):
         sql = 'select isbn, price, title, author, press, description, cover  from book_goods_info where %s=%s'
-        self.cursor.execute(sql, pair)
+        # self.cursor.execute(sql, [pair[0], pair[1]])
+        self.cursor.execute('select isbn, price, title, author, press, description, cover  from book_info where isbn="123"')
+        self.conn.commit()
         book=Book()
         bookpo_list = self.cursor.fetchall()
         booklist = []
+        print bookpo_list
         for bpo in bookpo_list:
+            print 'isbn = ', bpo[0]
             bookpo = BookPO()
-            bookpo['isbn'] = bpo[0]
-            bookpo['price'] = bpo[1]
-            bookpo['title'] = bpo[2]
-            bookpo['author'] = bpo[3]
-            bookpo['press'] = bpo[4]
-            bookpo['desciption'] = bpo[5]
-            bookpo['cover'] = bpo[6]
+            bookpo.isbn = bpo[0]
+            bookpo.price = bpo[1]
+            bookpo.title = bpo[2]
+            bookpo.author = bpo[3]
+            bookpo.press = bpo[4]
+            bookpo.desciption = bpo[5]
+            bookpo.cover = bpo[6]
 
-            sql = 'select isbn, link, platform, instant_price, crawling_time from book_goods_info where %s=%s'
-            self.cursor.execute(sql, ['isbn', bookpo['isbn']])
+            sql = 'select isbn, link, platform, instant_price, crawling_time from book_goods_info where %s="%s"'
+            #self.cursor.execute(sql, ['isbn', bookpo.isbn])
+            self.cursor.execute('select isbn, link, platform, instant_price, crawling_time from book_goods_info where isbn="123"')
+            print bookpo.isbn
+            self.conn.commit()
             goodspo_list = self.cursor.fetchall()
             for gpo in goodspo_list:
+                print 'haha'
                 goodspo = GoodsPO
-                goodspo['isbn'] = gpo[0]
-                goodspo['link'] = gpo[1]
-                goodspo['platform'] = gpo[2]
-                goodspo['instant_price'] = gpo[3]
-                goodspo['crawling_time'] = gpo[4]
+                goodspo.isbn = gpo[0]
+                goodspo.link = gpo[1]
+                goodspo.platform = gpo[2]
+                goodspo.instant_price = gpo[3]
+                goodspo.crawling_time = gpo[4]
                 booklist.append(self.__parse_po_to_book(bookpo, goodspo))
 
-    def query(self, pair, start_time, end_time):
+        return booklist
+
+    def query_by_time(self, pair, start_time, end_time):
         pass
 
     def __del__(self):
@@ -104,4 +123,11 @@ book_dao = BookDAO();
 #测试
 if __name__=="__main__":
     book = Book()
-    book_dao.insert(book);
+    book.isbn = "123"
+    book_dao.insert(book)
+    ad = ("isbn", "123")
+    print ad
+    a = book_dao.query(ad)
+    print a
+
+
