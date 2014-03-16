f858c82f0eafea1fbf0ee3622a8773f48148b5d5
yzx930420 <yzx930420@gmail.com>
2014-03-16 20:18:21 +0800

    修改对book_goods_info和book_info添加set_all方法

--- common/model/book_info.py
+++ common/model/book_info.py
@@ -12,3 +12,12 @@ class BookInfo:
         self.description= ''
         self.cover = ''
         self.goods_list = []
+
+    def set_all(self, list):
+        self.isbn = list[0]
+        self.price = list[1]
+        self.title = list[2]
+        self.author = list[3]
+        self.press = list[4]
+        self.desciption = list[5]
+        self.cover = list[6]
