6a1737cffd3d16461e201f1c927d7779ec543413
ArchMage <lfj200411@163.com>
2014-03-16 20:34:25 +0800

    debug jingdong scrapy complete

--- crawl/pipelines.py
+++ crawl/pipelines.py
@@ -1,6 +1,5 @@
 __author__ = 'Dazdingo'
 
-from common.model.book import Book
 from common.dao.book_dao import book_dao
 #import pymongo
 
@@ -19,20 +18,20 @@ class Pipeline(object):
         newbook.instant_price = item["instant"][0]
         newbook.link = item["url"]
         newbook.cover = item["img"][0]
-        #newbook.desc = item["desc"][0]
+        newbook.description = item["description"][0]
         newbook.platform = "dangdang"
         #newbook.time = ?
         book_dao.insert(newbook)
     #def process_item(self, item, spider):
     #    if(len(item['ISBN']) == 0):
     #    	return item
-    #    dbdata = {"name":"","price":"","ISBN":"","author":"","press":"","img":"","desc":""}
+    #    dbdata = {"name":"","price":"","ISBN":"","author":"","press":"","img":"","description":""}
     #    dbdata["name"] = item["name"][0]
     #    dbdata["price"] = item["price"][0]
     #    dbdata["ISBN"] = item["ISBN"][0]
     #    dbdata["author"] = item["author"][0]
     #    dbdata["press"] = item["press"][0]
     #    dbdata["img"] = item["img"][0]
-    #    dbdata["desc"] = item["desc"]
+    #    dbdata["description"] = item["description"]
     #    self.db.bookInfo.insert(dbdata)
     #    return item
