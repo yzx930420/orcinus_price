f636f5396c5ea0a2a23fdc6cdb8182c927c744ab
ArchMage <lfj200411@163.com>
2014-06-09 22:41:40 +0800

    add download delay and add user agent cookie

--- crawl/settings.py
+++ crawl/settings.py
@@ -9,7 +9,16 @@ ITEM_PIPELINES = {
     'crawl.pipelines.DetailPipeline': 800,
 }
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36'
+DOWNLOAD_DELAY = 2
+RANDOMIZE_DOWNLOAD_DELAY = True
+USER_AGENT = ['Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36',
+              'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
+              'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)',
+              'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2)',
+              'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+              'Mozilla/4.0 (compatible; MSIE 5.0; Windows NT)',
+              'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070309 Firefox/2.0.0.3']
+COOKIE_ENABLED = True
 #LOG_FILE = 'C:/Users/Administrator/Source/Repos/orcinus_price/crawl/error.txt'
 LOG_FILE = 'log/error.txt'
 LOG_ENABLED = False
