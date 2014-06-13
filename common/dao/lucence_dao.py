83cfb95e7f704675f19b1cd6e5c2466008ef2f11
XianSen <946007236@qq.com>
2014-06-13 20:58:58 +0800

    修改lucenceDAO的函数名

--- common/dao/lucence_dao.py
+++ common/dao/lucence_dao.py
@@ -7,7 +7,7 @@ class LucenceDAO:
     def query(self, action, keyword, index, size):
         return ['9787509611876','9780821228692','9787806886182']
 
-    def size(self, action, keyword):
+    def get_page_size(self, action, keyword):
         return 100;
 
     def __to_json(self,obj):
