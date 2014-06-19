0c7570e47ff7ddecae1078affc8e13ea15275212
ArchMage <lfj200411@163.com>
2014-06-19 21:44:03 +0800

    fixed bug in pipeline

--- crawl/pipelines.py
+++ crawl/pipelines.py
@@ -1,42 +1,46 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Dazdingo'
 
-import pymongo
+from scrapy.exceptions import DropItem
+from common.dao.book_dao import book_dao
+from common.model.book import Book
+from common.model.comment import Comment
+from common.dao.comment_dao import comment_dao
 
 
 class BookPipeline(object):
 
     @staticmethod
     def process_item(item, spider):
-        con = pymongo.Connection('localhost', 27017)
-        db = con.testspider
-        if item['platform'] == -1:
+        if item['platform'] == -1:      # not a book, drop it
             return item
-        if not item['ISBN']:
+        if not item['ISBN']:            # not a book, drop it
             item['platform'] = -1
             return item
         if item['platform'] == 3:       # if item is comments return to detail_pipeline
             return item
 
-        db_data = {"ISBN": "", "name": "", "price": "", "author": "",
-                   "press": "", "instant": "", "img": "", "description": ""}
-        if item['name']:
-            db_data['name'] = item['name'][0]
-        if item['price']:
-            db_data['price'] = item['price'][0]
-        if item['author']:
-            db_data['author'] = item['author'][0]
-        if item['press']:
-            db_data['press'] = item['press'][0]
-        if item['instant']:
-            db_data['instant'] = item['instant'][0]
-        if item['img']:
-            db_data['img'] = item['img'][0]
-        if item['description']:
-            db_data['description'] = item['description'][0]
-        db_data['ISBN'] = item['ISBN'][0]
-
-        db.booklist.insert(db_data)
+        new_book = Book()
+        if item["name"]:
+            new_book.title = item["name"][0]
+        if item["price"]:
+            new_book.price = item["price"][0]
+        if item["author"]:
+            new_book.author = item["author"][0]
+        if item["press"]:
+            new_book.press = item["press"][0]
+        if item["instant"]:
+            new_book.instant_price = item["instant"][0]
+        if item["img"]:
+            new_book.cover = item["img"][0]
+        if item["description"]:
+            new_book.description = item["description"][0]
+        new_book.isbn = item["ISBN"][0]
+        new_book.link = item["url"]
+        new_book.platform = item['platform']
+        #new_book.time = ?
+        new_book.platform = item['platform']
+        book_dao.insert(new_book)
         return item
 
 
@@ -44,17 +48,25 @@ class DetailPipeline(object):
 
     @staticmethod
     def process_item(item, spider):
-        con = pymongo.Connection('localhost', 27017)
-        db = con.testspider
+
         if item['platform'] != 3:
             return item
-        db_data = {"ISBN": "", "author": "", "detail": "", "comment_time": ""}
-        if item['ISBN']:
-            db_data['isbn'] = item['ISBN']
-        if item['author']:
-            db_data['author'] = item['author']
-        if item['detail']:
-            db_data['detail'] = item['detail'][0]
 
-        db.comment.insert(db_data)
+        if not item['ISBN']:
+            raise DropItem('item no isbn')
+
+        new_comment = Comment()
+
+        if item['ISBN']:
+            new_comment['isbn'] = item['ISBN']
+        if item['author']:
+            new_comment['author'] = item['author']
+        if item['detail']:
+            new_comment['detail'] = item['detail'][0]
+        if item['comment_time']:
+            new_comment['comment_time'] = item['comment_time'][0]
+
+        comment_dao.insert(new_comment)
         return item
+
+    pass
